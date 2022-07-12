01042ad826a656512371c8e6ed699d11e164e672
renzhao <renzhao@safeheron.com>
2022-07-12 15:08:14 +0800

    feat: 修改

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { FC, Suspense } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import routes from '@/routes'
 
 const App: FC = () => (
   <Router>
     <Suspense fallback={<div />}>
+      <div>
+        <Link to="/">home</Link>
+        <Link to="/about">about</Link>
+      </div>
       <Routes>
         {routes.map((route: any) => (
           <Route
